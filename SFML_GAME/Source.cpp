3abbd6131f352ac63567ea3056a0855fdb0598d6
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-03 20:36:14 +0700

    20

--- SFML_GAME/Source.cpp
+++ SFML_GAME/Source.cpp
@@ -1,4 +1,5 @@
-#include <SFML/Graphics.hpp>
+include <SFML / Graphics.hpp>
+#include <iostream>
 
 int main()
 {
@@ -9,8 +10,21 @@ int main()
 	collision.setPosition({ 200.f, 200.f });
 	collision.setFillColor(sf::Color::Red);
 
+	////// Texture
+	sf::Texture playerTexture;
+	if (!playerTexture.loadFromFile("res/img/healer_f.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+
+	////// Sprite
+	sf::Sprite shapeSprite;
+	shapeSprite.setTexture(playerTexture);
+	shapeSprite.setTextureRect(sf::IntRect(0, 0, 32, 38));
+
 	while (window.isOpen())
 	{
+		window.draw(shapeSprite);
 		window.draw(collision);
 		window.display();
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
