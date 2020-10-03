39d3de566248dee9ccb0ed3655aeaed059e96b4a
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-03 20:11:58 +0700

    0 %

--- SFML_GAME/Source.cpp
+++ SFML_GAME/Source.cpp
@@ -2,23 +2,20 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Game from scratch!");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	sf::CircleShape collision(100.f);
+	collision.setPosition({ 200.f, 200.f });
+	collision.setFillColor(sf::Color::Red);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+	while (window.isOpen())
+	{
+		window.display();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+		window.clear();
+	}
+	return 0;
 }
