4647d5c07fe414daf6e4df2e3140b7a17ed9bad1
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-10-04 17:16:20 +0700

    70 %

--- SFML_GAME/Source.cpp
+++ SFML_GAME/Source.cpp
@@ -18,7 +18,7 @@ int main()
 	// Sprite
 	sf::Sprite shapeSprite;
 	shapeSprite.setTexture(character.Princee);
-	shapeSprite.setTextureRect(sf::IntRect(0,0, 40, 54));
+	
 
 
 
@@ -36,23 +36,31 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
 			shapeSprite.move(.1f, 0.f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
 			shapeSprite.move(-.1f, 0.f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 3, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
 			shapeSprite.move(0.f, -.1f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
 			shapeSprite.move(0.f, .1f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 2, spriteSizeX, 32));
 		}
-		//STOP
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 		{
 			window.close();
+		}
+		animationFrame++;
+
+		if (animationFrame >= 2) {
+			animationFrame = 0;
 		}
 		window.clear();
 	}
